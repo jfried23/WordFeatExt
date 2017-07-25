15c14c95be399f6adc344a3043cc74f4f2276be5
Maggie Orton <vze52tg7@verizon.net>
2017-07-25 10:50:02 -0400

    add description

--- Tokenizer.py
+++ Tokenizer.py
@@ -2,18 +2,14 @@ import string
 from sklearn import feature_extraction, base
 
 class Tokenizer(base.BaseEstimator,base.TransformerMixin):
+    # pass in column name, data frame
+    # returns list of sparse matrix-style strings of tfidf values
     def __init__(self,colName):
         self.colName = colName
         
     def fit(self, X, *_): return self
     
     def tokenize(self, X):
-        """
-        sents = X[self.colName].tolist()
-        exclude = set(string.punctuation)
-        sents = [''.join(ch for ch in sent if ch not in exclude) for sent in sents]
-        tokenize_sents = [sent.replace("\n"," ").split(" ") for sent in sents]        
-        """
         exclude = set(string.punctuation)
         sent = ''.join(ch for ch in X if ch not in exclude)
         tokenize_sents = sent.replace("\n"," ").split(" ")
