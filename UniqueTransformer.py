d371a54e9d5391dd36484269a5f05ba2e1da921e
KQ Peng <kangqiao@cs.uchicago.edu>
2017-07-25 07:24:46 -0500

    docs and handling

--- UniqueTransformer.py
+++ UniqueTransformer.py
@@ -1,8 +1,25 @@
 from collections import Counter
+from sklearn import base
 
 class UniqueTransformer(base.BaseEstimator, base.TransformerMixin):
     """
-    Unique Transformer
+    Unique Transformer:
+
+    Transformer for use in scikit processing pipeline
+
+    Class methods:
+
+    fit(self, X, *_):
+        Takes: Input dataframe, variable arguments (dropped)
+        Returns: self
+
+    transform(self, X, *__):
+        Takes: Input dataframe, variable arguments (dropped)
+        Returns: Unique word count of every specificied column in the dataframe
+
+    fit_transform(self, X, *_):
+        Takes: Input dataframe, variable arguments (dropped)
+        Returns: self.transform(X)
     """
     def __init__(self, col_name):
         self.col_name = col_name
@@ -15,15 +32,18 @@ class UniqueTransformer(base.BaseEstimator, base.TransformerMixin):
         ret = []
         raw_str_list = X[self.col_name]
         for raw_str in raw_str_list:
-            word_list = re.sub(r"\W+", ' ', raw_str).lower().split()
-            the_count = Counter()
-            count_list = []
-            for word in word_list:
-                if len(word) != 1 or (word == 'a' or word == 'i'):
-                    count_list.append(word)
-                the_count.update(count_list)
-                
-            ret.append(len(the_count))
+            if type(raw_str) is str:
+                word_list = re.sub(r"\W+", ' ', raw_str).lower().split()
+                the_count = Counter()
+                count_list = []
+                for word in word_list:
+                    if len(word) != 1 or (word == 'a' or word == 'i'):
+                        count_list.append(word)
+                    the_count.update(count_list)
+                    
+                ret.append(len(the_count))
+            else:
+                ret.append(0)
 
         return ret
     
